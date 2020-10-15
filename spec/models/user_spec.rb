dba6678166187052d7fbc66d588c232bd3c6b028
Ben Daniels <benbanj@hotmail.com>
2020-10-15 18:43:23 +0100

    Adding more User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    subject(:user) { described_class.create(email: "email@email.com", 
+                                            password: "password",
+                                            first_name: "Harry",
+                                            last_name: "Harrison") }
+
+    context 'when valid' do
+      it 'is created with valid attributes' do
+        expect(user).to be_valid
+      end
+    end
+
+    context 'when invalid' do
+      it 'is not created without first and last name' do
+        user.first_name = nil
+        user.last_name = nil
+        expect(user).to_not be_valid
+      end
+    end
+  end
 end
