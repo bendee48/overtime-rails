73d7f785eff679f89dd953af3b04672758b4330f
Ben Daniels <benbanj@hotmail.com>
2020-10-22 16:38:02 +0100

    Adding admin authentication

---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin' do
+  describe 'admin dashboard' do
+    let(:user) { create(:user) }
+
+    context 'not logged in' do
+      it 'redirects attempt to access admin dashboard' do
+        visit admin_root_path
+        expect(current_path).to eql new_user_session_path
+      end
+    end
+
+    context 'logged in but not admin user' do
+      it 'redirects attempt to access admin dashboard' do
+        login_as(user, scope: :user)
+        visit admin_root_path
+        expect(current_path).to eql root_path
+      end
+    end
+
+    context 'logged in and admin user' do
+      it 'loads admin dashboard' do
+        admin_user = create(:admin_user)
+        login_as(admin_user, scope: :user)
+        visit admin_root_path
+        expect(current_path).to eql admin_root_path        
+      end
+    end
+  end
+end
