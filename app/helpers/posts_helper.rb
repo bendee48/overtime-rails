98138342177b1cdf94bbc05ea59fe1dd5ffa81f9
Ben Daniels <benbanj@hotmail.com>
2020-10-25 17:16:38 +0000

    Adding status label helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def status_tag(status)
+    content_tag(:span, "#{status}", class: "badge badge-#{labels[status]}")
+  end
+
+  def labels
+    {'submitted' => 'primary',
+     'approved' => 'success',
+     'rejected' => 'danger'}
+  end
 end
