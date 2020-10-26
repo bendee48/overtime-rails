54824c56815ffb8034f3f4651f63a1178984e919
Ben Daniels <benbanj@hotmail.com>
2020-10-26 20:05:30 +0000

    Implementing SmsTool/ twilio functionality

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,15 @@
 module SmsTool
+  account_sid = Rails.application.credentials.dig(:twilio, :account_sid)
+  auth_token = Rails.application.credentials.dig(:twilio, :auth_token)
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
   def self.send_sms(message:, number:)
     puts 'Sending sms...'
-    puts "#{message}: #{number}"
+    @client.messages.create(
+      from: Rails.application.credentials.dig(:twilio, :trial_number),
+      to: "+44#{number[1..-1]}",
+      body: "#{message}"
+    )
   end
 end
