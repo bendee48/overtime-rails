27db9f903640c36119c85ea5fdfc1cf4e737da92
Ben Daniels <benbanj@hotmail.com>
2020-10-27 10:43:21 +0000

    Adding AuditLog resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_many :audit_logs
 
   validates_presence_of :first_name, :last_name, :phone
   
