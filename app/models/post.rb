c817a73f22c249c91c46b0f644ecce2f78c8039a
Ben Daniels <benbanj@hotmail.com>
2020-10-28 21:48:08 +0000

    Removing pending audit if overtime requested

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   validates_presence_of :date, :rationale, :overtime_request
   validates :overtime_request, numericality: { greater_than: 0.0 }
+  after_save :update_audit_log
 
   scope :all_posts_of, ->(user) { where(user_id: user.id) }
+
+  private
+
+  def update_audit_log
+    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+    audit_log.confirmed! if audit_log
+  end
 end
+
