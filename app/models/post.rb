24f548988110d12895d4e8dc4ccf7049da102d65
Ben Daniels <benbanj@hotmail.com>
2020-10-30 15:36:26 +0000

    Adding callback for post rejections

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,22 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   validates_presence_of :date, :rationale, :overtime_request
   validates :overtime_request, numericality: { greater_than: 0.0 }
-  after_save :update_audit_log
 
   scope :all_posts_of, ->(user) { where(user_id: user.id) }
 
+  after_save :confirm_audit_log, if: :submitted?
+  after_save :un_confirm_audit_log, if: :rejected?
+
   private
 
-  def update_audit_log
+  def confirm_audit_log
     audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
     audit_log.confirmed! if audit_log
+  end
+
+  def un_confirm_audit_log
+    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+    audit_log.pending! if audit_log
   end
 end
 
